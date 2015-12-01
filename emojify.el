6d03c618aaa6f9864652afb1b4f1f7f37b634644
Iqbal Ansari <iqbalansari02@yahoo.com>
2015-12-01 23:39:41 +0530

    Check `redisplay-highlight-region-function` is bound before using it
    Restores compatibility with Emacs 24.3 :sweat_smile:

--- emojify.el
+++ emojify.el
@@ -637,7 +637,9 @@ and end of region respectively."
              (<= end emojify-region-end)))))
 
 (defun emojify--region-face (beg end)
-  (when (and (equal (default-value 'redisplay-highlight-region-function) redisplay-highlight-region-function)
+  ;; `redisplay-highlight-region-function' was not defined in Emacs 24.3
+  (when (and (or (not (boundp 'redisplay-highlight-region-function))
+                 (equal (default-value 'redisplay-highlight-region-function) redisplay-highlight-region-function))
              (or (emojify--inside-non-rectangle-selection-p beg end)
                  (emojify--inside-rectangle-selection-p beg end)))
     (face-background 'region)))
